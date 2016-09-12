e3219b67f84f929023d809c433c7baf50a3fa8f2
Peter-White <pwhite007@icloud.com>
2016-09-12 18:29:08 -0400

    Youtube search completed (°∀°)

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,51 +1,37 @@
 $(document).ready(function() {
 
+$('form').submit(function (e) {
+      e.preventDefault();
+      var $searchField = $('#search');
+      var $sumbitButton = $('#submit');
 
- // $('form').submit(function (e) {
- //    e.preventDefault();
- //    var $searchField = $('#search');
- //    var $sumbitButton = $('#submit');
-
- //    $searchField.prop("disabled",true);
- //    $sumbitButton.attr("disabled",true).val("searching....");
-
- //    // the AJAX part
- //    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
- //    var search = $searchField.val();
- //    var flickrOptions = {
- //      tags: search,
- //      format: "json"
- //    };
- //    function displayPhotos(data) {
- //      var photoHTML = '<ul>';
- //      $.each(data.items,function(i,photo) {
- //        photoHTML += '<li class="grid-25 tablet-grid-50">';
- //        photoHTML += '<a href="' + photo.link + '" class="image">';
- //        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
- //      }); // end each
- //      photoHTML += '</ul>';
- //      $('#photos').html(photoHTML);
- //         $searchField.prop("disabled",false);
- //         $sumbitButton.attr("disabled",false).val("Search");
- //    }
- //    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
- //  }); // end click
+      $searchField.prop("disabled",true);
+      $sumbitButton.attr("disabled",true).val("searching....");
 
       var $tubeTest = $('#tubeTest');
       var youtubeAPI = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCKQfeLQ1KcJox_KtjdLq_WYbbn97KbHPo";
+      var search = $searchField.val();
+
       var youtubeOptions = {
-          q: 'cat',
+          q: search,
           maxResults: 30,
       };
+
       function listStuff(data) {
         var youtubeStuff = '<ul>';
         $.each(data.items,function(i, video) {
-          youtubeStuff += '<li>';
-          youtubeStuff += video.snippet.title;
+          var videoUrl = video.id.videoId;
+          var videoTitle = video.snippet.title;
+          youtubeStuff += '<li class="grid-25 tablet-grid-50">';
+          youtubeStuff += '<iframe title="' + videoTitle + '" width="255" height="179" src="http://www.youtube.com/embed/' + videoUrl + '" frameborder="0" allowfullscreen></iframe>';
           youtubeStuff += '</li>';
         });
         youtubeStuff += '</ul>';
         $tubeTest.html(youtubeStuff);
+        $searchField.prop("disabled",false);
+        $sumbitButton.attr("disabled",false).val("Search");
       }
       $.getJSON(youtubeAPI, youtubeOptions, listStuff);
+    }); // form submit
 }); // end ready
+
