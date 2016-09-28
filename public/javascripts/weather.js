bf76d0ff6eefd3bdad6e6d53e7abe0114c8d76d7
Peter-White <pwhite007@icloud.com>
2016-09-28 11:23:21 -0400

    Weather GET test successful

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -1,3 +1,11 @@
-$(document.function(){
-
+$(document).ready(function() {
+  var $weathercontainer = $('#weathercontainer');
+  var weatherAPI = "http://api.wunderground.com/api/7c8568ecd192f9f6/conditions/q/MA/Boston.json";
+  function forecast(data) {
+    var weatherReport = '<h2>';
+    weatherReport += data.current_observation.image.url;
+    weatherReport += '</h2>';
+    $weathercontainer.html(weatherReport);
+  };
+  $.getJSON(weatherAPI, forecast);
 });
