6fe4e56e56f697411589ab1a2f8a3fc2fc27a017
Peter-White <pwhite007@icloud.com>
2016-09-28 12:50:32 -0400

    Translated Weather Icons to Plain English

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -2,9 +2,63 @@ $(document).ready(function() {
   var $weathercontainer = $('#weathercontainer');
   var weatherAPI = "http://api.wunderground.com/api/7c8568ecd192f9f6/conditions/q/MA/Boston.json";
   function forecast(data) {
-    var weatherReport = '<h2>';
-    weatherReport += data.current_observation.image.url;
-    weatherReport += '</h2>';
+
+  function forecastIcon() {
+    var icons = ["chanceflurries",
+      "chancerain",
+      "chancesleet",
+      "chancesnow",
+      "chancetstorms",
+      "clear",
+      "cloudy",
+      "flurries",
+      "fog",
+      "hazy",
+      "mostlycloudy",
+      "mostlysunny",
+      "partlycloudy",
+      "partlysunny",
+      "rain",
+      "sleet",
+      "snow",
+      "sunny",
+      "tstorms",
+      "unknown"];
+
+    var iconTrans = ["Chance of Flurries",
+      "Chance of Rain",
+      "Chance Freezing Rain",
+      "Chance of Snow",
+      "Chance of Thunderstorms",
+      "Clear",
+      "Cloudy",
+      "Flurries",
+      "Fog",
+      "Haze",
+      "Mostly Cloudy",
+      "Mostly Sunny",
+      "Partly Cloudy",
+      "Partly Sunny",
+      "Rain",
+      "Freezing Rain",
+      "Snow",
+      "Sunny",
+      "Thunderstorms",
+      "Unknown"];
+
+    for(var i = 0; i < icons.length; i++) {
+      if(data.current_observation.icon === icons[i]) {
+        return iconTrans[i];
+        break;
+      }
+    };
+  };
+
+
+    var weatherReport = '<img src="';
+    weatherReport += data.current_observation.icon_url;
+    weatherReport += '" />';
+    weatherReport += '<h2>' + forecastIcon() + '</h2>';
     $weathercontainer.html(weatherReport);
   };
   $.getJSON(weatherAPI, forecast);
