fc43a38afe1d16ae31360537d5880db109f85642
Peter-White <pwhite007@icloud.com>
2016-10-02 18:15:03 -0400

    Added Map Marker

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,8 +1,16 @@
 function initMap() {
   // Create a map object and specify the DOM element for display.
+  var coordinates = {lat: 42.332370, lng: -71.037130};
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 42.332438, lng: -71.037154},
+    center: coordinates,
     scrollwheel: true,
     zoom: 20
+  });
+
+  var marker = new google.maps.Marker({
+    position: coordinates,
+    map: map,
+    title: 'My Humble Abode'
   });
 }
